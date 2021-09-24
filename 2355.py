89163b6e35707bed952afbc75df8c5343af1ad49
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-24 08:45:27 -0300

    Create 2355.py

---
+++ 2355.py
@@ -0,0 +1,8 @@
+from math import floor as chao
+from math import ceil as cezin
+while True:
+    x=int(input())
+    if x==0:break
+    brasa=chao(x/90)
+    alema=cezin(7*x/90)
+    print(f"Brasil {brasa} x Alemanha {alema}")

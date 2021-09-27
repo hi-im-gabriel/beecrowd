9f7c287cdcba154a790fb670556ef4325369c60a
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-27 13:25:46 -0300

    Create 1041.py

---
+++ 1041.py
@@ -0,0 +1,8 @@
+x,y=map(float,input().split())
+if x==y==0:print("Origem")
+elif x==0:print("Eixo Y")
+elif y==0:print("Eixo X")
+elif x>0 and y>0:print("Q1")
+elif x<0 and y>0:print("Q2")
+elif x<0 and y<0:print("Q3")
+elif x>0 and y<0:print("Q4")

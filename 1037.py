b4f93f78e61ef5ee301a433327fd0679dad18ac3
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-27 13:25:11 -0300

    Create 1037.py

---
+++ 1037.py
@@ -0,0 +1,6 @@
+n=float(input())
+if n >= 0.00 and n <= 25.00:print("Intervalo [0,25]")
+elif n > 25.00 and n <= 50.00:print("Intervalo (25,50]")
+elif n>50.00 and n<=75.00:print("Intervalo (50,75]")
+elif n>75.00 and n <= 100.00:print("Intervalo (75,100]")
+else:print("Fora de intervalo")

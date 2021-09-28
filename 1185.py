09780bf917f3bdff28d8901b99d12048c668d538
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-28 11:03:43 -0300

    Create 1185.py

---
+++ 1185.py
@@ -0,0 +1,9 @@
+t=input()
+soma=aux=0
+for i in range(12):
+    for j in range(12):
+        n=float(input())
+        if i+j<=10:
+            soma+=n
+            aux+=1
+print("%.1f"%soma) if t=="S" else print("%.1f"%(soma/aux))

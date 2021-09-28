5e8aad10141deb2ee5fe892e2e61129737eb1d7f
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-28 09:29:28 -0300

    Update 1179.py

--- 1179.py
+++ 1179.py
@@ -1,6 +1,17 @@
-x=float(input())
-n=[]
-for i in range(100):
-    n.append(x)
-    x/=2
-for i in range(100):print("N[%d] = %.4f"%(i,n[i]))
+impar,par=[],[]
+for i in range(15):
+    n=int(input())
+    if n%2==0:par.append(n)
+    else:impar.append(n)
+    if len(par)==5:
+        for i in range(5):print(f"par[{i}] = {par[i]}")
+        par=[]
+    if len(impar)==5:
+        for i in range(5):print(f"impar[{i}] = {impar[i]}")
+        impar=[]
+tamP=len(par)
+tamI=len(impar)
+if tamI>0:
+    for i in range(tamI):print(f"impar[{i}] = {impar[i]}")
+if tamP>0:
+    for i in range(tamP):print(f"par[{i}] = {par[i]}")

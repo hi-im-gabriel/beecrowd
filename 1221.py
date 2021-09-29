a149d1a8de75ad08e56ec887a05fea9743a638b9
Gabriel Tavares <88493991+gabzin@users.noreply.github.com>
2021-09-29 11:23:32 -0300

    Update 1221.py

--- 1221.py
+++ 1221.py
@@ -1,11 +1,8 @@
-import math
+from math import sqrt
 def primo(n):
-    if n%2 == 0 and n>2:
-        return False
-    return all(n%i for i in range(3,int(math.sqrt(n))+1, 2))
+    if n%2==0 and n>2:return False
+    return all(n%i for i in range(3,int(sqrt(n))+1, 2))
 
-x=int(input())
-
-for i in range(0,x):
+for i in range(int(input())):
     n=int(input())
     print("Prime") if primo(n) else print("Not Prime")
